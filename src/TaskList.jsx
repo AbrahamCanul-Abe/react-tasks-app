77bf7c8bcbcd881fa25fc9767f6bd268d6a772df
Abraham Canul <abraham23.acc@gmail.com>
2023-01-28 01:49:52 -0600

    otra forma sin useEffect

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -2,11 +2,11 @@ import { tasks as data } from './tasks';
 import { useState, useEffect } from 'react';
 
 function TaskList() {
-	const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState(data);
 
-	useEffect(() => {
+	/* useEffect(() => {
 		setTasks(data);
-	}, []);
+	}, []); */
 
 	if (tasks.length === 0) {
 		return <h1>No hay tareas aun</h1>;
